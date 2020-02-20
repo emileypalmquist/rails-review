04afb39fc13e9b008e6ba4e7c948268c632b3fbf
emileypalmquist <emiley.palmquist@icloud.com>
2020-02-20 15:07:02 -0800

    initial commit with customer and drink resources

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Drink.create(name: 'Coffee' description: 'THE BEST DRINK EVER')
+Drink.create(name: 'Tea' description: 'tastes like perfume')
+Drink.create(name: 'Water' description: 'the miracle drink')
+Drink.create(name: 'Beer' descritption: 'liquid gold')
+
+10.times do 
+    Customer.create(name: Faker::Name.name, drink_id: rand(1..4))
+end
